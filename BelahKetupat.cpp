df82ecaaa0d7bb0ff791776f62b5719b095ea471
Faizaditya-oss <faizaditya750@gmail.com>
2026-03-02 20:19:07 +0700

    Menambahkan var global dan prosedur

---
+++ BelahKetupat.cpp
@@ -0,0 +1,14 @@
+// buatlah sebuah program menghitung luas belah ketupat terdapat struktur prosedur input fungsi menghitung luas.
+// menggunakan parameter dan prosedur output. rumus 1/2 * diagonal 1 * diagonal 2
+#include <iostream>
+using namespace std;
+
+float d1, d2;
+
+void input() {
+    cout << "Masukkan diagonal 1: ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2: ";
+    cin >> d2;
+}
+
